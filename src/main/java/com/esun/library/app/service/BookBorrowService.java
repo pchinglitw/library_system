4c29639fad923dcd66a3c975cacb4dd597ebca00
pchinglitw <pchinglitw@gmail.com>
2024-03-16 23:29:00 +0800

    update book borrow function

--- src/main/java/com/esun/library/app/service/BookBorrowService.java
+++ src/main/java/com/esun/library/app/service/BookBorrowService.java
@@ -1,4 +1,53 @@
 package com.esun.library.app.service;
 
+import com.esun.library.domain.entity.Book;
+import com.esun.library.domain.entity.Inventory;
+import com.esun.library.domain.service.BookService;
+import com.esun.library.domain.service.InventoryService;
+import com.esun.library.web.dto.request.BookRequest;
+import jakarta.persistence.EntityNotFoundException;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestClientException;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+
+@Service
 public class BookBorrowService {
+    @Autowired
+    private BookService bookService;
+    @Autowired
+    private InventoryService inventoryService;
+
+    public void execute(BookRequest request) {
+        Integer userId = request.getUserId();
+        Integer inventoryId = request.getInventoryId();
+
+        if (userId == null || inventoryId == null) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        }
+
+        Optional<Inventory> inventoryOpt = inventoryService.findInventoryById(inventoryId);
+        if (inventoryOpt.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "inventoryId not found");
+        }
+
+        Inventory inventory = inventoryOpt.get();
+
+        if (!StringUtils.equals(inventory.getStatus().getStatusId(), "1")) {
+            throw new ResponseStatusException(HttpStatus.CONFLICT, "The status is currently not ready for borrowing books.");
+        }
+
+        try {
+            inventoryService.bookBorrow(inventoryId, request.getUserId());
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+    }
 }
--- src/main/java/com/esun/library/web/dto/response/BookResponse.java
+++ src/main/java/com/esun/library/web/dto/response/BookResponse.java
@@ -1,33 +1,14 @@
 package com.esun.library.web.dto.response;
 
+import lombok.Data;
+
+@Data
 public class BookResponse {
+    private String msg;
+
     private String isbn;
 
     private String name;
 
     private String author;
-
-    public String getIsbn() {
-        return isbn;
-    }
-
-    public void setIsbn(String isbn) {
-        this.isbn = isbn;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getAuthor() {
-        return author;
-    }
-
-    public void setAuthor(String author) {
-        this.author = author;
-    }
 }
