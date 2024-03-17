31a127fe506a06b7443e95c0d54cba14bbe420d1
pchinglitw <pchinglitw@gmail.com>
2024-03-17 17:42:39 +0800

    set crossOrigin in UserController

--- src/main/java/com/esun/library/web/controller/UserController.java
+++ src/main/java/com/esun/library/web/controller/UserController.java
@@ -7,14 +7,12 @@ import com.esun.library.web.dto.request.RegisterRequest;
 import com.esun.library.web.dto.response.UserResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequestMapping("/user")
+@CrossOrigin(origins = "http://localhost:5173")
 public class UserController {
     private final UserRegisterService registerService;
     private final UserLoginService loginService;
